732ccf4803dc7f2cdcba61600433d2c456015ab7
Nabb0 <90761381+Nabb0@users.noreply.github.com>
2023-09-26 11:29:02 +0200

    Aggiunta meteo previsto

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import pyowm
+from datetime import datetime, timedelta
 
 # Crea una finestra Tkinter
 root = tk.Tk()
@@ -10,46 +11,51 @@ larghezza_finestra = 1000  # Larghezza in pixel
 altezza_finestra = 800  # Altezza in pixel
 root.geometry(f"{larghezza_finestra}x{altezza_finestra}")
 
-# Funzione per ottenere le informazioni meteo di Milano
-def get_milan_weather():
-    owm = pyowm.OWM('2de7c19ab75bb0d7182f2a46cea6859a')
- 
-    location = "Milano, IT"  # Imposta la località su Milano
-    
+# Funzione per ottenere le informazioni meteo di una città
+def get_weather(city):
+    owm = pyowm.OWM('2de7c19ab75bb0d7182f2a46cea6859a')  # Inserisci qui la tua chiave API di OpenWeatherMap
+
     try:
         mgr = owm.weather_manager()
-        observation = mgr.weather_at_place(location)
+        observation = mgr.weather_at_place(city)
         weather = observation.weather
         temperature = weather.temperature('celsius')['temp']
         status = weather.status
 
-        frame1_label.config(text=f"Milano\nTemperature: {temperature}°C\nStatus: {status}")
+        frame1_label.config(text=f"{city}\nTemperature: {temperature}°C\nStatus: {status}")
+
+        # Ottenere le previsioni per oggi
+        forecast = mgr.forecast_at_place(city, '3h')
+        today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        tomorrow = today + timedelta(days=1)
+        weather_forecast = forecast.get_weather_at(tomorrow)
+        temperature_forecast = weather_forecast.temperature('celsius')['temp']
+
+        frame2_label.config(text=f"{city}\nTemperature Forecast Today: {temperature_forecast}°C")
     except Exception as e:
         frame1_label.config(text=f"Error: {e}")
-    
+
 # Creazione dei widget
-frame1 = tk.Frame(root, bg="lightblue", width=400, height=300)
-frame1.grid(row=0, column=0, rowspan=2, columnspan=2)
+frame1 = tk.Frame(root, bg="lightblue")
+frame1.place(relx=0, rely=0, relwidth=0.5, relheight=0.5)
 
 frame1_label = tk.Label(frame1, text="", padx=10, pady=10)
 frame1_label.pack()
 
+frame2 = tk.Frame(root, bg="lavender")
+frame2.place(relx=0.5, rely=0, relwidth=0.5, relheight=0.5)
 
-frame2 = tk.Frame(root, bg="lavender", width=larghezza_finestra // 2, height=altezza_finestra // 2)
-frame2.grid(row=0, column=2, rowspan=2, columnspan=2)
+frame2_label = tk.Label(frame2, text="", padx=10, pady=10)
+frame2_label.pack()
 
-frame3 = tk.Frame(root, bg="#7e99dd", width=larghezza_finestra // 2, height=altezza_finestra // 2)
-frame3.grid(row=2, column=0, rowspan=2, columnspan=2)
+frame3 = tk.Frame(root, bg="#7e99dd")
+frame3.place(relx=0, rely=0.5, relwidth=0.5, relheight=0.5)
 
-frame4 = tk.Frame(root, bg="orange", width=larghezza_finestra // 2, height=altezza_finestra // 2)
-frame4.grid(row=2, column=2, rowspan=2, columnspan=2)
+frame4 = tk.Frame(root, bg="orange")
+frame4.place(relx=0.5, rely=0.5, relwidth=0.5, relheight=0.5)
 
 # Organizzazione dei widget nella finestra
-get_milan_weather()  # Chiamata iniziale per ottenere i dati meteo di Milano
-# Rimuovi il campo di input e il pulsante di ricerca, poiché ora mostriamo solo i dati di Milano
-# label.pack()
-# entry.pack()
-# search_button.pack()
+get_weather("Milano")  # Chiamata iniziale per ottenere i dati meteo di Milano
 
 # Avvia il loop principale di Tkinter
 root.mainloop()
