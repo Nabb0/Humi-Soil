c8dd2ff712f8dc78ab42c3a50df755edbc210ef5
Nabb0 <90761381+Nabb0@users.noreply.github.com>
2023-09-28 10:54:46 +0200

    Update CodSperimentale.py

--- CodSperimentale.py
+++ CodSperimentale.py
@@ -11,14 +11,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 
-# Funzione per generare e salvare un numero casuale
-def generate_and_save_random_number():
+def generate_and_save_random_number_with_time():
     random_number = random.randint(1, 10)
-    print(random_number)
+    current_time = datetime.datetime.now()
     
-    # Scrivi il numero casuale in un file
+    # Formatta l'orario nel formato desiderato, ad esempio "2023-09-28 14:30:00"
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Scrive il numero casuale e l'orario nel file
     with open("numeri_casuali.txt", "a") as file:
-        file.write(str(random_number) + "\n")
+        file.write(f"{formatted_time}: {random_number}\n")
     
     # Controlla se il numero è pari o dispari e controlla il LED
     if random_number % 2 == 0:
@@ -126,6 +128,11 @@ frame4.place(relx=0.5, rely=0.5, relwidth=0.5, relheight=0.5)
 # Crea un widget Label per visualizzare l'immagine meteo
 label = tk.Label(root)
 label.pack()
+
+# Crea un pulsante Tkinter per generare il numero casuale con l'orario
+random_button = tk.Button(root, text="Genera Numero Casuale", command=generate_and_save_random_number_with_time)
+random_button.pack()
+
 
 # Organizzazione dei widget nella finestra
 get_weather("Milan")  # Chiamata iniziale per ottenere i dati meteo di Milano
